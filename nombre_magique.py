d02acb219c386520e92e6bc6ee7e81f2a8da7af2
Murielle-SIMO <mmemefo@gmail.com>
2022-08-01 01:30:22 +0200

    ajout du fichier nombre_magique.py

--- nombre_magique.py
+++ nombre_magique.py
@@ -25,28 +25,6 @@ def demander_nombre(nb_min, nb_max):
 
     return nombre_int
 
-"""def trouver_nombre_magique(nb_min, nb_max):
-    nombre = 0
-    vies = NB_VIES
-    while not nombre == NOMBRE_MAGIQUE and vies > 0:
-        print(f"vous avez {vies} vies")
-        nombre = demander_nombre(nb_min, nb_max)
-
-        if nombre > NOMBRE_MAGIQUE:
-            print("Le nombre magique est plus petit")
-            vies -= 1
-
-        elif nombre < NOMBRE_MAGIQUE :
-            print("le nombre magique est plus grand")
-            vies -= 1
-
-        else:
-            print("Bravo, vous avez gagné")
-
-    if vies == 0:
-        print(f"vous avez perdu! Le nombre magique était : {NOMBRE_MAGIQUE}.")"""
-
-
 
 def trouver_nombre_magique(nb_min, nb_max):
 
